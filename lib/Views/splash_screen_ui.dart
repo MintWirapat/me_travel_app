470210432cbb6932f8f4d7a6c4bed591c3d6b59b
MintWirapat <s6452710002@sau.ac.th>
2023-03-28 15:58:40 +0700

    Code V.1.1 by Wirapat Chongsakul get code splash,login , register

--- lib/Views/splash_screen_ui.dart
+++ lib/Views/splash_screen_ui.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:me_travel_app/Views/Login_ui.dart';
 
 class SplashScreenUI extends StatefulWidget {
   const SplashScreenUI({super.key});
@@ -9,9 +11,57 @@ class SplashScreenUI extends StatefulWidget {
 
 class _SplashScreenUIState extends State<SplashScreenUI> {
   @override
+  void initState() {
+    Future.delayed(
+      Duration(
+        seconds: 3,
+      ),
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LoginUI(),
+          ),
+        );
+      },
+    );
+  }
+
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: MediaQuery.of(context).size.width * 0.3,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 0.5,
+            ),
+            Text(
+              'บันทึกการเดินทาง',
+              style: GoogleFonts.kanit(
+                fontSize: MediaQuery.of(context).size.width * 0.08,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 0.5,
+            ),
+            Text(
+              'Created by : 6452710002\nWirapat Chongsakul',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.kanit(
+                fontSize: MediaQuery.of(context).size.width * 0.04,
+                color: Color.fromARGB(255, 55, 55, 55),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
